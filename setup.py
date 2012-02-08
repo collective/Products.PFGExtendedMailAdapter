ed5b974472fb6af0789f5155a5d791bacbc32149
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-02-08 20:44:16 +0200

    file deletend and dependencies updated

--- setup.py
+++ setup.py
@@ -4,53 +4,45 @@ from setuptools import setup
 import os
 
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-version = read('Products', 'PFGExtendedMailAdapter', 'version.txt').strip()
-
 long_description = (
     open("README.txt").read() + "\n" +
     open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
     open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
     open(os.path.join("docs", "CONTRIBUTORS.txt")).read() + "\n" +
     open(os.path.join("docs", "CREDITS.txt")).read()
-    )
+)
 
-setup(name='Products.PFGExtendedMailAdapter',
-      version=version,
-      description="This package extends mail adapter content type of Products.PloneFormGen.",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='Products.PFGExtendedMailAdapter',
+    version='1.1',
+    description="This package extends mail adapter content type of Products.PloneFormGen.",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Taito Horiuchi',
-      author_email='taito.horiuchi@gmail.com',
-      url='http://pypi.python.org/pypi/Products.PFGExtendedMailAdapter',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'Products.PloneFormGen',
-          'hexagonit.testing',
-          'plone.app.testing',
-          'plone.browserlayer',
-          'mock',
-          'setuptools',
-          'unittest2',
-          'zope.i18nmessageid',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='',
+    author='Taito Horiuchi',
+    author_email='taito.horiuchi@gmail.com',
+    url='https://github.com/taito/Products.PFGExtendedMailAdapter',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Products.PloneFormGen',
+        'hexagonit.testing',
+        'plone.browserlayer',
+        'setuptools',
+        'zope.i18nmessageid',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
