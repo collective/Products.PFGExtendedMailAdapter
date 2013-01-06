58dd37f848d3369a0e60bee6b57ceb08d05a5a70
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-01-06 05:50:53 +0200

    Release 2.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = (
 
 setup(
     name='Products.PFGExtendedMailAdapter',
-    version='2.2',
+    version='2.3',
     description="Adds extended mail adapter to Products.PloneFormGen.",
     long_description=long_description,
     classifiers=[
